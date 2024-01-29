cb4a3058795fb9035faad22da97da1989cb70ccf
Joaoof <joaodeus400@gmail.com>
2024-01-29 17:28:33 -0300

    fix: class '?'

--- ApiCatalogo/Models/Category.cs
+++ ApiCatalogo/Models/Category.cs
@@ -3,6 +3,6 @@
     {
     public int CategoryId { get; set; }
 
-    public string Name { get; set; }
-    public string ImageUrl { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
 }
