00fa138584919022b5379d5aa30e8d4910640471
Joaoof <joaodeus400@gmail.com>
2024-01-31 09:17:53 -0300

    feat: Aplication Data Annotations

--- ApiCatalogo/Models/Category.cs
+++ ApiCatalogo/Models/Category.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ApiCatalogo.Models;
+    [Table("Categories")]    
     public class Category
     {
 
@@ -8,9 +11,15 @@ namespace ApiCatalogo.Models;
     {
         Products = new Collection<Product>();
     } // Boa pratica inicializar a Collection
+    [Key]
     public int CategoryId { get; set; }
 
+    [Required]
+    [StringLength(80)]
     public string? Name { get; set; }
+
+    [Required]
+    [StringLength(300)]
     public string? ImageUrl { get; set; }
 
     public ICollection<Product>? Products { get; set; } // Relacionamento
