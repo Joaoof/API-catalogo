8904c69a8209354bc5faec71a19c32555b579e3a
Joaoof <joaodeus400@gmail.com>
2024-01-31 15:58:01 -0300

    feat: add Controller ProductsController

---
+++ ApiCatalogo/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using ApiCatalogo.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context; // instância 
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        } // instância do context injetada no controlador
+    }
+}
