1abc5259bd1c7a84856908accf89f8e6efb62d7c
Joaoof <joaodeus400@gmail.com>
2024-02-01 09:52:44 -0300

    feat: instance injectable

---
+++ ApiCatalogo/Controllers/CategoriesController.cs
@@ -0,0 +1,18 @@
+﻿using ApiCatalogo.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
