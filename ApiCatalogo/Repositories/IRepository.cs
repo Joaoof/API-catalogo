2b72d0bbe6c1ac02a4db8b3c27ba446d999c8d64
Joaoof <joaodeus400@gmail.com>
2024-02-14 16:04:24 -0300

    feat: Implementing the Generic Repository

---
+++ ApiCatalogo/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ApiCatalogo.Repositories
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+
+        T? Get(Expression<Func<T, bool>> predicate); // aceita como argumento uma expressão lambda
+
+        T Create(T entity);
+        
+        T Update(T entity);
+
+        T Delete(T entity);
+    }
+}
