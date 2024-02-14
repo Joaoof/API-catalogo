b7f84314dd64987d8c30e1578dc76d9c83e24c44
Joaoof <joaodeus400@gmail.com>
2024-02-14 16:34:18 -0300

    feat: Implementing the Generic Repository - II

---
+++ ApiCatalogo/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Interfaces;
+using System.Linq.Expressions;
+
+namespace ApiCatalogo.Repositories
+{
+    public class Repository<T>: IRepository<T> where T : class
+    {
+        protected readonly AppDbContext _context;
+        
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList(); // O método Set<T>() é usado para obter um conjunto (ou tabela) de entidades do tipo T no contexto. Ele retorna um objeto do tipo DbSet<T>, que representa a coleção de entidades associadas a uma tabela específica no banco de dados.
+        }
+
+        public T? Get(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().FirstOrDefault(predicate);
+        }
+
+        public T Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+
+            return entity;
+        }
+
+        public T Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+
+            return entity;
+        }
+    }
+}
